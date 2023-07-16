10b725608439227237352860cfeb6054eea69e55
krishmk1009 <krushnamk1009@gmail.com>
2023-07-16 17:27:04 +0530

    user_update commited

--- public/profile.php
+++ public/profile.php
@@ -2,7 +2,32 @@
  
 $user_id = get_current_user_id();
 $fname = get_usermeta($user_id, "first_name");
+$imagePath = get_usermeta($user_id , "user_profile_img_path");
+$imageUrl = get_usermeta($user_id , "user_profile_img_url");
+
+
 if(isset($_POST["Update"])) {
+    
+
+        if($_FILES["user_img"]["error"] ==0){
+            $file = $_FILES["user_img"];
+            echo "<pre>";
+            // print_r($file);
+            $ext= explode("/" , $file["type"])[1];
+            $file_name= "$user_id.$ext";
+
+        $image = wp_upload_bits($file_name , null ,file_get_contents($file["tmp_name"]) );
+
+            if(!metadata_exists('user' , $user_id , 'user_profile_img_url')){
+                add_user_meta($user_id , "user_profile_img_url" , $image['url']);
+                add_user_meta($user_id , "user_profile_img_path" , esc_sql($image['file']));
+                // print_r($image);
+            }else{
+                update_user_meta($user_id , "user_profile_img_url" , $image['url']);
+                update_user_meta($user_id , "user_profile_img_path" , esc_sql($image['file']));
+                // print_r($image);
+            }
+        }
     $fname = esc_sql($_POST["user_fname"]);
     $userdata = array(
         "ID" => $user_id,
@@ -19,12 +44,24 @@ if(isset($_POST["Update"])) {
         $fname = get_usermeta($user_id, "first_name");
     }
 }
+// echo $imagePath;
+?>
+
+<?php
+
+if($imageUrl !=""){
+    ?>
+<img src=" <?php echo $imageUrl ; ?>" alt="<?php echo $imageUrl ; ?>" />
+    <?php
+}
 ?>
 
 <h1>Hello: <?php echo" $fname $user_id"; ?></h1>
 
 <div>
-    <form action="" method="POST">
+    <form action="" method="POST" enctype="multipart/form-data">
+        
+        Profile Image: <input type="file" name="user_img" id="user_img"  /> <br/>
         Name: <input type="text" name="user_fname" id="fname_input" value="<?php echo $fname; ?>" /> 
         <input type="hidden" value="<?php echo $user_id; ?>" name="user_id" />
         <input type="submit" id="submit_btn" name="Update" />
